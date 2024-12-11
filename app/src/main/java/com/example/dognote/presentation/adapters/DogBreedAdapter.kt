885727ed4b533499a6ff1cd88ae647ff18bf9a09
ASrdkn <uyalina098i@gmail.com>
2024-12-11 16:18:42 +0700

    DogBreedAdapter

---
+++ app/src/main/java/com/example/dognote/presentation/adapters/DogBreedAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.dognote.presentation.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dognote.data.models.DogBreed
+
+// Адаптер для RecyclerView, который отображает элементы списка пород собак
+class DogBreedAdapter(
+    private val context: Context, // Контекст для запуска активностей
+    private val dogItems: List<DogBreed> // Список элементов, которые будут отображаться (только породы собак)
+) : RecyclerView.Adapter<DogBreedAdapter.DogBreedViewHolder>() {
+
+    // Внутренний класс для привязки одного элемента (ViewHolder)
+    inner class DogBreedViewHolder(private val binding: ItemDogBreedBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(dogItem: DogBreed) {
+            with(binding) {
+                dogName.text = dogItem.name
+                dogNote.visibility = View.GONE
+                Glide.with(root)
+                    .load(dogItem.image.url)
+                    .placeholder(R.drawable.placeholder_dog_image)
+                    .into(dogImage)
+            }
+
+            // Обработчик клика по элементу списка
+            binding.root.setOnClickListener {
+                val intent = Intent(context, SecondActivity::class.java).apply {
+                    putExtra("DOG_BREED_NAME", dogItem.name) // Передаем имя породы
+                    putExtra("DOG_IMAGE_URL", dogItem.image.url) // Передаем URL изображения породы
+                }
+                context.startActivity(intent) // Запускаем новую активность с переданными данными
+            }
+        }
+    }
+
+    // Метод для создания нового ViewHolder (элемента списка)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogBreedViewHolder {
+        // Создаем привязку для элемента списка (элемент разметки)
+        val binding = ItemDogBreedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DogBreedViewHolder(binding) // Возвращаем новый ViewHolder
+    }
+
+    // Метод для привязки данных к ViewHolder
+    override fun onBindViewHolder(holder: DogBreedViewHolder, position: Int) {
+        holder.bind(dogItems[position]) // Привязываем данные из списка к элементу
+    }
+
+    // Метод для получения количества элементов в списке
+    override fun getItemCount(): Int = dogItems.size
+}
