f0acbe22e1a98c819f4ee58f9737180b74505718
ASrdkn <uyalina098i@gmail.com>
2024-12-11 15:57:58 +0700

    Настройка проекта

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -13,7 +14,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -33,15 +33,37 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.10.0")
+    // Основные библиотеки AndroidX
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    // Jetpack Lifecycle (ViewModel и LiveData)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+    // Retrofit для работы с REST API
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    // Glide для загрузки изображений
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    // Room для локальной базы данных
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
+    ksp(libs.room.compiler)
+    // Dagger для Dependency Injection
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
+    // Библиотеки для тестов
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
