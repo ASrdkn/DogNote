bf4bc9445dd8a9c215ccf684c9f4b1bbc3a2cd30
ASrdkn <uyalina098i@gmail.com>
2025-05-16 19:14:09 +0700

    Рефакторинг на MVVM: перенос логики из SecondActivity в SecondActivityViewModel

--- app/src/main/java/com/example/dognote/presentation/viewmodel/SecondActivityViewModel.kt
+++ app/src/main/java/com/example/dognote/presentation/viewmodel/SecondActivityViewModel.kt
@@ -1,4 +1,32 @@
 package com.example.dognote.presentation.viewmodel
 
-class SecondActivityViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.dognote.data.models.DogNote
+import com.example.dognote.data.repository.AppDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SecondActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val database = AppDatabase.getDatabase(application)
+
+    // LiveData для отслеживания успеха или ошибки при сохранении
+    private val _saveStatus = MutableLiveData<String>()
+    val saveStatus: LiveData<String> get() = _saveStatus
+
+    // Функция для сохранения заметки в базе данных
+    fun saveDogNote(dogNote: DogNote) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                database.dogNoteDao().insertDogNote(dogNote)
+                _saveStatus.postValue("Заметка сохранена")
+            } catch (e: Exception) {
+                _saveStatus.postValue("Ошибка при сохранении")
+            }
+        }
+    }
 }
