ad81006b0dd1ce2291cc1931b7545d2846c01be0
ASrdkn <uyalina098i@gmail.com>
2025-05-16 18:49:28 +0700

    Рефакторинг на MVVM: изменение DogNoteViewModel

--- app/src/main/java/com/example/dognote/presentation/viewmodel/DogNoteViewModel.kt
+++ app/src/main/java/com/example/dognote/presentation/viewmodel/DogNoteViewModel.kt
@@ -3,26 +3,51 @@ package com.example.dognote.presentation.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.MutableLiveData
+import com.example.dognote.data.api.RetrofitClient
+import com.example.dognote.data.models.DogBreed
 import com.example.dognote.data.models.DogNote
 import com.example.dognote.data.repository.AppDatabase
 import com.example.dognote.data.repository.DogNoteDao
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DogNoteViewModel(application: Application) : AndroidViewModel(application) {
 
     private val dogNoteDao: DogNoteDao = AppDatabase.getDatabase(application).dogNoteDao()
 
-    // Функция для получения всех записей о собаках
+    // LiveData для данных из базы данных
     fun getAllDogNotes(): LiveData<List<DogNote>> {
-        // Возвращаем LiveData, которая будет автоматически обновляться
         return dogNoteDao.getAllNotes()
     }
 
-    fun insertNote(note: DogNote) {
-        viewModelScope.launch(Dispatchers.IO) {
-            dogNoteDao.insertDogNote(note)
-        }
+    // LiveData для данных о породах собак из API
+    private val _dogBreeds = MutableLiveData<List<DogBreed>>()
+    val dogBreeds: LiveData<List<DogBreed>> get() = _dogBreeds
+
+    // LiveData для ошибок
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
+
+    // Функция для загрузки данных о породах собак из API
+    fun fetchDogBreeds() {
+        val apiKey = "live_ejKtuMsWVvvZRY9iJMKAGMnDQUFEvUR78gsJe1TnZGPtd2UzY60eHgDo2Co5HxDO"
+
+        RetrofitClient.dogApiService.getDogBreeds(apiKey).enqueue(object : Callback<List<DogBreed>> {
+            override fun onResponse(call: Call<List<DogBreed>>, response: Response<List<DogBreed>>) {
+                if (response.isSuccessful) {
+                    // Когда данные получены, обновляем LiveData
+                    _dogBreeds.postValue(response.body())
+                }  else {
+                    _error.postValue("Ошибка при получении данных") // Обновляем LiveData с ошибкой
+                }
+            }
+
+            override fun onFailure(call: Call<List<DogBreed>>, t: Throwable) {
+                _error.postValue("Ошибка: ${t.message}")
+            }
+        })
     }
+
 }
