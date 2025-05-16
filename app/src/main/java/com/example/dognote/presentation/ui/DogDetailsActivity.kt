235e51d5787932f751ed3bc1a2768d3ff34e1a2d
ASrdkn <uyalina098i@gmail.com>
2025-05-16 19:12:22 +0700

    Рефакторинг на MVVM: изменение DogDetailsActivity

--- app/src/main/java/com/example/dognote/presentation/ui/DogDetailsActivity.kt
+++ app/src/main/java/com/example/dognote/presentation/ui/DogDetailsActivity.kt
@@ -3,20 +3,20 @@ package com.example.dognote.presentation.ui
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.example.dognote.R
-import com.example.dognote.data.repository.AppDatabase
 import com.example.dognote.data.models.DogNote
-import com.example.dognote.data.repository.DogNoteDao
 import com.example.dognote.databinding.ActivityDogDetailsBinding
-import kotlinx.coroutines.launch
+import com.example.dognote.presentation.viewmodel.DogDetailsViewModel
 
 class DogDetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDogDetailsBinding
-    private lateinit var dogNoteDao: DogNoteDao
+    private val dogDetailsViewModel: DogDetailsViewModel by viewModels()
+
     private var currentDogId: Int = -1
     private var isEditing: Boolean = false
     private var currentImageUrl: String? = null
@@ -24,22 +24,39 @@ class DogDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //Инициализация ViewBinding
+        // Инициализация ViewBinding
         binding = ActivityDogDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        dogNoteDao = AppDatabase.getDatabase(this).dogNoteDao()
+        // Получение данных о собаке
         currentDogId = intent.getIntExtra("DOG_ID", -1)
 
-        //Проверка ID на корректность
         if (currentDogId != -1) {
-            loadDogDetails(currentDogId)
+            dogDetailsViewModel.loadDogDetails(currentDogId)
         } else {
             Toast.makeText(this, "Error loading dog details", Toast.LENGTH_SHORT).show()
             finish()
         }
 
-        //Обработка редактирования
+        // Наблюдение за изменениями в данных
+        dogDetailsViewModel.dogNote.observe(this, Observer { dogNote ->
+            // Проверка на null перед вызовом метода displayDogDetails
+            dogNote?.let {
+                displayDogDetails(it)
+            } ?: run {
+                Toast.makeText(this, "Dog details not found", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Наблюдение за статусом операции (сохранение или удаление)
+        dogDetailsViewModel.status.observe(this, Observer { message ->
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            if (message == "Заметка обновлена" || message == "Запись удалена") {
+                finish()
+            }
+        })
+
+        // Обработка редактирования
         binding.editIcon.setOnClickListener {
             if (isEditing) {
                 saveDogNote()
@@ -48,66 +65,39 @@ class DogDetailsActivity : AppCompatActivity() {
             }
         }
 
-        //Удаление заметки
+        // Удаление заметки
         binding.deleteIcon.setOnClickListener {
-            deleteDogNote()
+            dogDetailsViewModel.deleteDogNote(currentDogId)
         }
 
-        //Чекбокс
+        // Чекбокс
         binding.sawDogCheckBox.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.dogNicknameInput.visibility = View.VISIBLE
-            } else {
-                binding.dogNicknameInput.visibility = View.GONE
-            }
+            binding.dogNicknameInput.visibility = if (isChecked) View.VISIBLE else View.GONE
         }
     }
 
-    //Загрузка данных о собаке
-    private fun loadDogDetails(dogId: Int) {
-        lifecycleScope.launch {
-            val dogNote = dogNoteDao.getDogNoteById(dogId)
-            if (dogNote != null) {
-                currentImageUrl = dogNote.imageUrl
-                displayDogDetails(dogNote)
-            }
-        }
-    }
-
-    //Отображение данных о собаке
+    // Отображение данных о собаке
     private fun displayDogDetails(dogNote: DogNote) {
+        currentImageUrl = dogNote.imageUrl
         with(binding) {
             dogBreedName.text = dogNote.breedName
             noteInput.setText(dogNote.note)
             dogNicknameInput.setText(dogNote.nickname)
             sawDogCheckBox.isChecked = dogNote.sawDog
 
-            //Видимость поля ввода клички
-            if (dogNote.sawDog) {
-                dogNicknameInput.visibility = View.VISIBLE
-            } else {
-                dogNicknameInput.visibility = View.GONE
-            }
+            // Видимость поля ввода клички
+            dogNicknameInput.visibility = if (dogNote.sawDog) View.VISIBLE else View.GONE
 
             Glide.with(this@DogDetailsActivity).load(dogNote.imageUrl).into(dogImage)
 
-            //Отключение редактирования при просмотре
+            // Отключение редактирования при просмотре
             noteInput.isEnabled = false
             dogNicknameInput.isEnabled = false
             sawDogCheckBox.isEnabled = false
         }
     }
 
-    //Удаление заметки
-    private fun deleteDogNote() {
-        lifecycleScope.launch {
-            dogNoteDao.deleteDogNoteById(currentDogId)
-            Toast.makeText(this@DogDetailsActivity, "Запись удалена", Toast.LENGTH_SHORT).show()
-            finish()
-        }
-    }
-
-    //Режим редактирования
+    // Режим редактирования
     private fun enableEditing() {
         isEditing = true
         with(binding) {
@@ -118,25 +108,21 @@ class DogDetailsActivity : AppCompatActivity() {
         }
     }
 
-    //Сохранение
+    // Сохранение изменений
     private fun saveDogNote() {
         val updatedNote = binding.noteInput.text.toString()
         val updatedNickname = binding.dogNicknameInput.text.toString()
         val sawDogChecked = binding.sawDogCheckBox.isChecked
 
-        lifecycleScope.launch {
-            val updatedDogNote = DogNote(
-                id = currentDogId,
-                breedName = binding.dogBreedName.text.toString(),
-                note = updatedNote,
-                sawDog = sawDogChecked,
-                nickname = updatedNickname,
-                imageUrl = currentImageUrl ?: ""
-            )
+        val updatedDogNote = DogNote(
+            id = currentDogId,
+            breedName = binding.dogBreedName.text.toString(),
+            note = updatedNote,
+            sawDog = sawDogChecked,
+            nickname = updatedNickname,
+            imageUrl = currentImageUrl
+        )
 
-            dogNoteDao.updateDogNote(updatedDogNote)
-            Toast.makeText(this@DogDetailsActivity, "Заметка обновлена", Toast.LENGTH_SHORT).show()
-            finish()
-        }
+        dogDetailsViewModel.saveDogNote(updatedDogNote)
     }
 }
