6d5ae2e2d5ccc087e6b2dcbf3e8ae66aa27d7fc5
ASrdkn <uyalina098i@gmail.com>
2025-05-20 15:27:57 +0700

    Рефакторинг на MVVM+добавление поиска: добавление метода поиска

--- app/src/main/java/com/example/dognote/data/api/DogApiService.kt
+++ app/src/main/java/com/example/dognote/data/api/DogApiService.kt
@@ -4,8 +4,11 @@ import com.example.dognote.data.models.DogBreed
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface DogApiService {
     @GET("breeds")
     fun getDogBreeds(@Header("x-api-key") apiKey: String): Call<List<DogBreed>>
+    @GET("breeds/search")
+    fun searchDogs(@Header("x-api-key") apiKey: String, @Query("q") query: String): Call<List<DogBreed>>
 }
